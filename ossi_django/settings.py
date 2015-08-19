e70cdd062bfaabc61a88fa3b24727acbc7f2611c
Keith Funkhouser <keith.funkhouser@gmail.com>
2015-08-19 17:46:40 -0500

    moved static files to s3

--- ossi_django/settings.py
+++ ossi_django/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django_filters',
     'address',
     'crispy_forms',
+    'storages',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -180,3 +181,27 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
+# django-storages settings
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'Cache-Control': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'ossi'
+AWS_ACCESS_KEY_ID = os.environ.get('OSSI_S3_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('OSSI_S3_SECRET')
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
