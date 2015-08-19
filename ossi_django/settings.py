4729654232c68c50de30c2225a3b9db2d27aca4b
Keith Funkhouser <keith.funkhouser@gmail.com>
2015-08-18 19:37:07 -0500

    MEDIA_ROOT

--- ossi_django/settings.py
+++ ossi_django/settings.py
@@ -172,6 +172,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
